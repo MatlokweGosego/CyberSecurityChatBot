f76f1d4cb152cc7f83dab01c3c1bcdfb2bd0712a
MatlokweGosego <ST10437390@rcconnect.edu.za>
2025-03-27 20:23:53 +0000

    Created the greeting class and added the voice greeting

---
+++ Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Media;
+
+namespace CyberSecurityChatBot
+{
+    public  class Greeting
+    {
+        public Greeting()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\RC_Student_lab\source\repos\CyberSecurityChatBot\CyberSecurityAwarenessChatbotGreeting.wav");
+            player.PlaySync();
+            Console.WriteLine("Hello!Welcome to the Cubersecurity Chatbot. I am your Assistant");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace CyberSecurityChatBot
 {
@@ -10,6 +11,7 @@ namespace CyberSecurityChatBot
     {
         static void Main(string[] args)
         {
+            new Greeting() { };
         }
     }
 }
