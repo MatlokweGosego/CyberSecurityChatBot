97566aa3e119b4840d128b126dfa4ce5e31bbaa9
MatlokweGosego <ST10437390@rcconnect.edu.za>
2025-03-30 23:10:24 +0000

    Addes the ASCII Art to the greeting class

--- Greeting.cs
+++ Greeting.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Media;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CyberSecurityChatBot
 {
-    public  class Greeting
+    public class Greeting
     {
         public Greeting()
         {
             SoundPlayer player = new SoundPlayer(@"C:\Users\RC_Student_lab\source\repos\CyberSecurityChatBot\CyberSecurityAwarenessChatbotGreeting.wav");
             player.PlaySync();
-          Console.WriteLine("Hello!Welcome to the Cubersecurity Chatbot. I am your Assistant");
+            DisplayASCIIArt(@"C:\Users\RC_Student_lab\source\repos\CyberSecurityChatBot\abstract-cyber-security-logo-line-blue-light-shield-icon-linear-style-with-eye-lens-camera-combination-isolated-on-blue-background-flat-logo-design-template-element-vector.jpg");
+        }
+        public string GetUserName()
+        {
+            Console.WriteLine("Hello!Welcome to the Cubersecurity Chatbot. I am your Assistant");
             Console.WriteLine("What is your name?:)");
             string userName = Console.ReadLine();
+
             if (string.IsNullOrWhiteSpace(userName))
             {
                 Console.WriteLine("Sorry, I did not quite get that. Can you please write your name below?");
-               userName = Console.ReadLine();
+                userName = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(userName)) {
                     Console.WriteLine("Thats okay, I will call you User1");
                     userName = "User1";
@@ -25,7 +32,31 @@ namespace CyberSecurityChatBot
             return userName;
         }
 
+        private void DisplayASCIIArt(string imagePath)
+        {
+            string characters = "@#$%&*!:'.' ";
+            try
+            {
+                Bitmap bitmap = new Bitmap(imagePath);
+                int width = 100;
+                int height = (bitmap.Height * width) / bitmap.Width;
+                Bitmap resized = new Bitmap(bitmap, new Size(width, height));
+
+                for (int y = 0; y < resized.Height; y++)
+                {
+                    for (int x = 0; x < resized.Width; x++)
+                    {
+                        Color pixelColor = resized.GetPixel(x, y);
+                        int gray = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                        int index = (gray * characters.Length - 1) / 255;
+                        Console.Write(characters[index]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+    }
     }
 }
-
-//we are supposed to add the ASCII image here and ensure that it displays as it should:) After hours? as well as the decorative s
